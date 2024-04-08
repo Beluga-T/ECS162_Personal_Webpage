f0b16159231fcb19c0b823df0e865ac4ca902d72
beluga-T <tswlovexyb@outlook.com>
2024-04-08 10:15:08 -0700

    Fix formatting and content in index.html and script.js files

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //interaction: Console log when the page is fully loaded
-document.addEventListener('DOMContentLoaded', function() {
-    
+document.addEventListener('DOMContentLoaded', function () {
+
     console.log('Page fully loaded and DOM is ready');
-    
+
 });
